21baf36ae393309291f70b5155f2202add729f78
Dominik Schoeffmann <schoeffm@in.tum.de>
2016-05-16 20:17:18 +0200

    add aes decryption

--- aes.cpp
+++ aes.cpp
@@ -27,14 +27,23 @@ int main() {
 		cout << "IV:" << endl;
 		hexdump(iv.bits_of().data(), 16);
 
-		Pipe pipe(get_cipher("AES-128/CBC", key, iv, ENCRYPTION));
-
-		pipe.process_msg("secrets1234567890ABCabc");
-
-		secure_vector<byte> c1 = pipe.read_all(0);
+		// Encrypt
+		Pipe enc(get_cipher("AES-128/CBC", key, iv, ENCRYPTION));
+		enc.process_msg("secrets1234567890ABCabc");
+		secure_vector<byte> enc_vec = enc.read_all(0);
 
 		cout << "Encrypted:" << endl;
-		hexdump(c1.data(), c1.size());
+		hexdump(enc_vec.data(), enc_vec.size());
+
+		// Decrypt
+		Pipe dec(get_cipher("AES-128/CBC", key, iv, DECRYPTION));
+		dec.process_msg(enc_vec);
+		secure_vector<byte> dec_vec = dec.read_all(0);
+
+		cout << "Decrypted:" << endl;
+		hexdump(dec_vec.data(), dec_vec.size());
+		string dec_str((const char*) dec_vec.data(), dec_vec.size());
+		cout << "Decrypted String: " << dec_str << endl;
 	}
 	catch(std::exception& e) {
 		cerr << e.what() << endl;
